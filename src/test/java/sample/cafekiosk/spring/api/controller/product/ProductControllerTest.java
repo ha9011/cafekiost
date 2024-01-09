63c8b75306378a144801d8dd81780322904a2694
ha9011 <gkehddnjs90@gmail.com>
2024-01-09 22:50:25 +0900

    @EnableJpaAuditing 분리

--- src/test/java/sample/cafekiosk/spring/api/controller/product/ProductControllerTest.java
+++ src/test/java/sample/cafekiosk/spring/api/controller/product/ProductControllerTest.java
@@ -1,12 +1,20 @@
 package sample.cafekiosk.spring.api.controller.product;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import sample.cafekiosk.spring.api.controller.product.request.CreateProductRequest;
 import sample.cafekiosk.spring.api.service.product.ProductService;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,14 +24,27 @@ class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean // 가짜객체 빈을 컨테이너에 등록하게 해줌 (ProductController 에 격납된 객체들과 동일하게...)
     private ProductService productService;
 
     @Test
     @DisplayName("신규상품을 등록한다.")
-    void createProduct() {
+    void createProduct() throws Exception {
         // given
+        CreateProductRequest req = CreateProductRequest.builder()
+                .type(ProductType.HANDMADE)
+                .sellingStatus(ProductSellingStatus.SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
 
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/products/new")
+                .content(objectMapper.writeValueAsString(req))
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(MockMvcResultMatchers.status().isOk());
         // when
 
         // then
